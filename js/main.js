beab9aab9bb1139a72e1d5c89f72d209b566d00d
bongomin <bbongomindaniel@gmail.com>
2019-05-07 08:53:52 +0300

    refactered code for the portfolio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-/* Flexslider */
+/* Flexslider it controls the shape of the first interface with the footer */
 $(window).load(function() {
   "use strict";
 	$('.flexslider').flexslider({
@@ -17,29 +17,3 @@ $(window).load(function() {
 	});
 });
 
-/* Mixitup Portfolio */
-jQuery(document).ready(function($) {
-  "use strict";
-	$('#portfolio').mixitup({
-		targetSelector: '.item',
-		transitionSpeed: 450
-	});
-});
-
-/* Nivo - Lightbox */
-jQuery(document).ready(function($) {
-  "use strict";
-    $('.nivo-lbox').nivoLightbox({ effect: 'fade' });
-});
-
-/* Skills  loading bar*/
-jQuery(document).ready(function($) {
-	"use strict";
-	$('.skills-info').appear(function() {
-	$('.skill1').css('width', '71%');
-	$('.skill2').css('width', '85%');
-	$('.skill3').css('width', '76%');
-	$('.skill4').css('width', '53%');
-	$('.skill5').css('width', '69%');
-	},{accX: 0, accY: -150});
-});
